5cee2ae855a566f54a4878c045e57ccc528ca728
Douglas Soffi <douglas.soffi@protonmail.com>
2025-10-01 16:48:10 -0300

    Finalizando capitulo 1 e realizando desafios

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector('h1');
+h1.innerHTML = 'Jogo do número secreto';
+
+let p = document.querySelector('p');
+p.innerHTML = 'Escolha um número de 1 a 10';
+
+function verificarChute() {
+    console.log('Funciona');
+}
